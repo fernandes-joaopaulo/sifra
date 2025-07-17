e88bdf6ec88b2d4053fb393c439c4ab3f122d883
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2025-07-17 16:53:39 -0300

    Adiciona componente Button

---
+++ src/components/Button/Button.js
@@ -0,0 +1,16 @@
+import "./Button.css";
+
+export default function Button(props) {
+    return (
+        <>
+            <input 
+            className="btn"
+            type="button" 
+            value={props.value}
+            style={{
+                backgroundColor: `${props.backgroundColor}`,
+                color: `${props.color}`,
+            }}/>
+        </>
+    );
+}
