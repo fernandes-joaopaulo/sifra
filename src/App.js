12b9719649e7475ab3e0c6570a45d8770c3e5727
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2025-07-18 14:21:39 -0300

    Adiciona App.js ao gitignore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import './App.css';
-import Dashboard from "./users/Dashboard";
-
-import icon1 from "./assets/icons/check-list.png";
-import icon2 from "./assets/icons/consulta.png";
-import icon3 from "./assets/icons/info.png";
-import icon4 from "./assets/icons/alunos.png";
-import icon5 from "./assets/icons/editar.png";
 
 function App() {
 
-  const cards = [
-    {label: "Lançar Frequência", icon: icon1},
-    {label: "Consultar Frequência", icon: icon2},
-    {label: "Minhas informações", icon: icon3},
-    {label: "Registro de alunos", icon: icon4}
-  ];
-
   return (
     <div className='app'>
-      <Dashboard cards={cards} username={"Alessandra"}/>
+
     </div>
   );
 }
--- src/components/Perfil/Perfil.js
+++ src/components/Perfil/Perfil.js
@@ -3,12 +3,12 @@ import './Perfil.css';
 export default function Perfil(props){
 
     return(
-        <div className="user" style={{display: `${props.display}`}}>
-            <img src={props.image} alt="Foto do usuário"/>
+        <div className="user">
+            <img className='user-img' src={props.image} alt="Foto do usuário"/>
             <div className='bottom'>
                 <p>{props.username}</p>
-                <div className="btn">
-                   <img src={props.icon} alt="Ícone"/>
+                <div className="btn-icon" style={{display: `${props.display}`}}>
+                   <img src={props.imageicon} alt="Ícone"/>
                 </div>
             </div>
         </div>
