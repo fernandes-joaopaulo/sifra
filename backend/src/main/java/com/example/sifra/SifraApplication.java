a7925417a44b4dd97ba7e48da00a80e9c401c4c9
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2025-07-20 15:14:48 -0300

    Divide projeto em frontend e backend com JavaSpring

---
+++ backend/src/main/java/com/example/sifra/SifraApplication.java
@@ -0,0 +1,13 @@
+package com.example.sifra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SifraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SifraApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/sifra/SifraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sifra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SifraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
